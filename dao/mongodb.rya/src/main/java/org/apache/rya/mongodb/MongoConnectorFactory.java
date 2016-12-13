d53e3c19f93253dcd3905751e8845340b934737e
Caleb Meier <caleb.meier@parsons.com>
2016-12-13 12:26:29 -0800

    RYA-144-ExternalSetMatchingFramework

--- dao/mongodb.rya/src/main/java/org/apache/rya/mongodb/MongoConnectorFactory.java
+++ dao/mongodb.rya/src/main/java/org/apache/rya/mongodb/MongoConnectorFactory.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,27 +16,23 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.rya.mongodb;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Objects;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import javax.annotation.concurrent.ThreadSafe;
 
 import org.apache.commons.configuration.ConfigurationRuntimeException;
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.http.annotation.ThreadSafe;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoException;
 import com.mongodb.ServerAddress;
 
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
-
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
 /**
  * Mongo convention generally allows for a single instance of a {@link MongoClient}
  * throughout the life cycle of an application.  This MongoConnectorFactory lazy
@@ -44,68 +40,38 @@ import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
  * is invoked.
  */
 @ThreadSafe
-@ParametersAreNonnullByDefault
+@DefaultAnnotation(NonNull.class)
 public class MongoConnectorFactory {
-
-    private static MongodForTestsFactory testsFactory = null;
-
     private static MongoClient mongoClient;
 
     private final static String MSG_INTRO = "Failed to connect to MongoDB: ";
 
     /**
-     * @param conf The {@link Configuration} defining how to construct the MongoClient. (not null)
+     * @param conf The {@link Configuration} defining how to construct the MongoClient.
      * @return A {@link MongoClient}.  This client is lazy loaded and the same one
      * is used throughout the lifecycle of the application.
      * @throws IOException - if MongodForTestsFactory constructor has an io exception.
      * @throws ConfigurationRuntimeException - Thrown if the configured server, port, user, or others are missing.
      * @throws MongoException  if can't connect despite conf parameters are given
      */
-    public static synchronized MongoClient getMongoClient(final Configuration conf) throws ConfigurationRuntimeException, MongoException {
-        Objects.requireNonNull(conf);
-
+    public static synchronized MongoClient getMongoClient(final Configuration conf)
+            throws ConfigurationRuntimeException, MongoException {
         if (mongoClient == null) {
-            // The static client has not yet created, is it a test/mock instance, or a service?
-            if (conf.getBoolean(MongoDBRdfConfiguration.USE_TEST_MONGO, false)) {
-                createMongoClientForTests();
-            } else {
-                createMongoClientForServer(conf);
-            }
+            createMongoClientForServer(conf);
         }
         return mongoClient;
     }
 
     /**
-     * Shuts down any resources that are being held by this factory.
+     * Silently closes the underlying Mongo client.
      */
-    public static synchronized void shutdown() {
-        if(mongoClient != null) {
-            mongoClient.close();
-            mongoClient = null;
-        }
-
-        if(testsFactory != null) {
-            testsFactory.shutdown();
-            testsFactory = null;
-        }
+    public static synchronized void closeMongoClient() {
+        IOUtils.closeQuietly(mongoClient);
+        mongoClient = null;
     }
 
     /**
-     * Create a local temporary MongoDB instance and client object and assign it to this class's static mongoClient
-     * @throws MongoException  if can't connect
-     */
-    private static void createMongoClientForTests() throws MongoException {
-        try {
-            testsFactory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
-            mongoClient = testsFactory.newMongo();
-        } catch (final IOException e) {
-            // Rethrow as an unchecked error.  Since we are in a test mode here, just fail fast.
-            throw new MongoException(MSG_INTRO+"creating a factory for a test/mock MongoDB instance.",e);
-        }
-    }
-
-    /**
-     * Create a MongoDB client object and assign it to this class's static mongoClient
+     * Create a MongoDB client object and assign it to this class's static mongoClient 
      * @param conf configuration containing connection parameters
      * @throws ConfigurationRuntimeException - Thrown if the configured server, port, user, or others are missing.
      * @throws MongoException  if can't connect despite conf parameters are given
@@ -115,7 +81,7 @@ public class MongoConnectorFactory {
         // Connect to a running Mongo server
         final String host = requireNonNull(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE), MSG_INTRO+"host name is required");
         final int port = requireNonNullInt(conf.get(MongoDBRdfConfiguration.MONGO_INSTANCE_PORT), MSG_INTRO+"Port number is required.");
-        final ServerAddress server = new ServerAddress(host, port);
+        ServerAddress server = new ServerAddress(host, port);
         // check for authentication credentials
         if (conf.get(MongoDBRdfConfiguration.MONGO_USER) != null) {
             final String username = conf.get(MongoDBRdfConfiguration.MONGO_USER);
@@ -135,13 +101,13 @@ public class MongoConnectorFactory {
 
     /**
      * Throw exception for un-configured required values.
-     *
+     * 
      * @param required  String to check
      * @param message  throw configuration exception with this description
      * @return unaltered required string
      * @throws ConfigurationRuntimeException  if required is null
      */
-    private static String requireNonNull(String required, String message) throws ConfigurationRuntimeException {
+    private static String requireNonNull(final String required, final String message) throws ConfigurationRuntimeException {
         if (required == null) {
             throw new ConfigurationRuntimeException(message);
         }
@@ -151,13 +117,13 @@ public class MongoConnectorFactory {
     /*
      * Same as above, check that it is a integer and return the parsed integer.
      */
-    private static int requireNonNullInt(String required, String message) throws ConfigurationRuntimeException {
+    private static int requireNonNullInt(final String required, final String message) throws ConfigurationRuntimeException {
         if (required == null) {
             throw new ConfigurationRuntimeException(message);
         }
         try {
             return Integer.parseInt(required);
-        } catch (final NumberFormatException e) {
+        } catch (NumberFormatException e) {
             throw new ConfigurationRuntimeException(message);
         }
     }
