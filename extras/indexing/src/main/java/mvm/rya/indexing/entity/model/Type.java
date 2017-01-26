apache/rya

RYA-144 Mongo DB Entity index.

extras/indexing/src/main/java/mvm/rya/indexing/entity/model/Type.java
@@ -1,0 +1,102 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package mvm.rya.indexing.entity.model;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Objects;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.concurrent.Immutable;
+
+import com.google.common.collect.ImmutableSet;
+
+import mvm.rya.api.domain.RyaURI;
+import mvm.rya.indexing.entity.storage.TypeStorage;
+
+/**
+ * Defines the structure of an {@link TypedEntity}.
+ * </p>
+ * For example, suppose you want a {@link Type} that defines what properties are
+ * available for icecream. It could be modeled like this:
+ * <pre>
+ *                 Type ID: &lt;urn:icecream>
+ *              Properties: &lt;urn:brand>
+ *                          &lt;urn:flavor>
+ *                          &lt;urn:ingredients>
+ *                          &lt;urn:nutritionalInformation>
+ * </pre>
+ */
+@Immutable
+@ParametersAreNonnullByDefault
+public class Type {
+
+    /**
+     * Uniquely identifies the Type within a {@link TypeStorage}.
+     */
+    private final RyaURI id;
+
+    /**
+     * The names of {@link Property}s that may be part of an {@link TypedEntity} of this type.
+     */
+    private final ImmutableSet<RyaURI> propertyNames;
+
+    /**
+     * Constructs an instance of {@link Type}.
+     *
+     * @param id - Uniquely identifies the Type within a {@link TypeStorage}. (not null)
+     * @param propertyNames - The names of {@link Property}s that may be part of an {@link TypedEntity} of this type. (not null)
+     */
+    public Type(final RyaURI id, final ImmutableSet<RyaURI> propertyNames) {
+        this.id = requireNonNull(id);
+        this.propertyNames = requireNonNull(propertyNames);
+    }
+
+    /**
+     * @return Uniquely identifies the Type within a {@link TypeStorage}.
+     */
+    public RyaURI getId() {
+        return id;
+    }
+
+    /**
+     * @return The names of {@link Property}s that may be part of an {@link TypedEntity} of this type.
+     */
+    public ImmutableSet<RyaURI> getOptionalFieldNames() {
+        return propertyNames;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, propertyNames);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o instanceof Type) {
+            final Type type = (Type) o;
+            return Objects.equals(id, type.id) &&
+                    Objects.equals(propertyNames, type.propertyNames);
+        }
+        return false;
+    }
+}

extras/indexing/src/main/java/mvm/rya/indexing/entity/storage/mongo/TypeDocumentConverter.java
@@ -1,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package mvm.rya.indexing.entity.storage.mongo;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import org.bson.Document;
+
+import com.google.common.collect.ImmutableSet;
+
+import mvm.rya.api.domain.RyaURI;
+import mvm.rya.indexing.entity.model.Type;
+
+/**
+ * Converts between {@link Type} and {@link Document}.
+ */
+@ParametersAreNonnullByDefault
+public class TypeDocumentConverter implements DocumentConverter<Type> {
+
+    public static final String ID = "_id";
+    public static final String PROPERTY_NAMES = "propertyNames";
+
+    @Override
+    public Document toDocument(final Type type) {
+        requireNonNull(type);
+
+        final Document doc = new Document();
+        doc.append(ID, type.getId().getData());
+
+        final List<String> propertyNames = new ArrayList<>();
+        type.getOptionalFieldNames().forEach(field -> propertyNames.add(field.getData()));
+        doc.append(PROPERTY_NAMES, propertyNames);
+
+        return doc;
+    }
+
+    @Override
+    public Type fromDocument(final Document document) throws DocumentConverterException {
+        requireNonNull(document);
+
+        if(!document.containsKey(ID)) {
+            throw new DocumentConverterException("Could not convert document '" + document +
+                    "' because its '" + ID + "' field is missing.");
+        }
+
+        if(!document.containsKey(PROPERTY_NAMES)) {
+            throw new DocumentConverterException("Could not convert document '" + document +
+                    "' because its '" + PROPERTY_NAMES + "' field is missing.");
+        }
+
+        final RyaURI typeId = new RyaURI( document.getString(ID) );
+
+        final ImmutableSet.Builder<RyaURI> propertyNames = ImmutableSet.builder();
+        ((List<String>) document.get(PROPERTY_NAMES))
+            .forEach(propertyName -> propertyNames.add(new RyaURI(propertyName)));
+
+        return new Type(typeId, propertyNames.build());
+    }
+}

ejwhite922/incubator-rya

extras/indexing/src/main/java/mvm/rya/indexing/entity/model/Type.java
@@ -1,0 +1,102 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package mvm.rya.indexing.entity.model;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Objects;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.concurrent.Immutable;
+
+import com.google.common.collect.ImmutableSet;
+
+import mvm.rya.api.domain.RyaURI;
+import mvm.rya.indexing.entity.storage.TypeStorage;
+
+/**
+ * Defines the structure of an {@link TypedEntity}.
+ * </p>
+ * For example, suppose you want a {@link Type} that defines what properties are
+ * available for icecream. It could be modeled like this:
+ * <pre>
+ *                 Type ID: &lt;urn:icecream>
+ *              Properties: &lt;urn:brand>
+ *                          &lt;urn:flavor>
+ *                          &lt;urn:ingredients>
+ *                          &lt;urn:nutritionalInformation>
+ * </pre>
+ */
+@Immutable
+@ParametersAreNonnullByDefault
+public class Type {
+
+    /**
+     * Uniquely identifies the Type within a {@link TypeStorage}.
+     */
+    private final RyaURI id;
+
+    /**
+     * The names of {@link Property}s that may be part of an {@link TypedEntity} of this type.
+     */
+    private final ImmutableSet<RyaURI> propertyNames;
+
+    /**
+     * Constructs an instance of {@link Type}.
+     *
+     * @param id - Uniquely identifies the Type within a {@link TypeStorage}. (not null)
+     * @param propertyNames - The names of {@link Property}s that may be part of an {@link TypedEntity} of this type. (not null)
+     */
+    public Type(final RyaURI id, final ImmutableSet<RyaURI> propertyNames) {
+        this.id = requireNonNull(id);
+        this.propertyNames = requireNonNull(propertyNames);
+    }
+
+    /**
+     * @return Uniquely identifies the Type within a {@link TypeStorage}.
+     */
+    public RyaURI getId() {
+        return id;
+    }
+
+    /**
+     * @return The names of {@link Property}s that may be part of an {@link TypedEntity} of this type.
+     */
+    public ImmutableSet<RyaURI> getPropertyNames() {
+        return propertyNames;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, propertyNames);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o instanceof Type) {
+            final Type type = (Type) o;
+            return Objects.equals(id, type.id) &&
+                    Objects.equals(propertyNames, type.propertyNames);
+        }
+        return false;
+    }
+}

extras/indexing/src/main/java/mvm/rya/indexing/entity/storage/mongo/TypeDocumentConverter.java
@@ -1,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package mvm.rya.indexing.entity.storage.mongo;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import org.bson.Document;
+
+import com.google.common.collect.ImmutableSet;
+
+import mvm.rya.api.domain.RyaURI;
+import mvm.rya.indexing.entity.model.Type;
+
+/**
+ * Converts between {@link Type} and {@link Document}.
+ */
+@ParametersAreNonnullByDefault
+public class TypeDocumentConverter implements DocumentConverter<Type> {
+
+    public static final String ID = "_id";
+    public static final String PROPERTY_NAMES = "propertyNames";
+
+    @Override
+    public Document toDocument(final Type type) {
+        requireNonNull(type);
+
+        final Document doc = new Document();
+        doc.append(ID, type.getId().getData());
+
+        final List<String> propertyNames = new ArrayList<>();
+        type.getPropertyNames().forEach(field -> propertyNames.add(field.getData()));
+        doc.append(PROPERTY_NAMES, propertyNames);
+
+        return doc;
+    }
+
+    @Override
+    public Type fromDocument(final Document document) throws DocumentConverterException {
+        requireNonNull(document);
+
+        if(!document.containsKey(ID)) {
+            throw new DocumentConverterException("Could not convert document '" + document +
+                    "' because its '" + ID + "' field is missing.");
+        }
+
+        if(!document.containsKey(PROPERTY_NAMES)) {
+            throw new DocumentConverterException("Could not convert document '" + document +
+                    "' because its '" + PROPERTY_NAMES + "' field is missing.");
+        }
+
+        final RyaURI typeId = new RyaURI( document.getString(ID) );
+
+        final ImmutableSet.Builder<RyaURI> propertyNames = ImmutableSet.builder();
+        ((List<String>) document.get(PROPERTY_NAMES))
+            .forEach(propertyName -> propertyNames.add(new RyaURI(propertyName)));
+
+        return new Type(typeId, propertyNames.build());
+    }
+}